bf7702b548b151b850b7c071aa7681c670af14a2
mohammed-aftab <mohammedaftabvali@gmail.com>
2025-07-29 10:22:55 +0530

    Abstarct Class

--- src/Abstraction/taks1.java
+++ src/Abstraction/taks1.java
@@ -2,24 +2,41 @@ package Abstraction;
 
 abstract class Mobile{
      abstract void call();
+     abstract void message();
 
      void openYouTube(){
          System.out.println("the YouTube is opened now!!!!");
+     }
+     void broadcast(){
+         System.out.println("Broadcasted a message to everyone in my contact👍👍👍");
      }
 
 }
 
 class Aftab extends Mobile{
     void call(){
-        System.out.println("Call to Aftab");
+        System.out.println("Call to Aftab 👋");
+    }
+    void message(){
+        System.out.println("Drop a Message to Aftab");
     }
 }
 class Priya extends Mobile{
     void call(){
-        System.out.println("call to priya");
+        System.out.println("call to priya 🤞 ");
+    }
+    void message(){
+        System.out.println("Drop a Message to Priya");
     }
 }
-
+class Abdul_sir extends Mobile{
+    void call(){
+        System.out.println("call to Abdul_sir 🙏");
+    }
+    void message(){
+        System.out.println("Drop a Message to Abdul_sir");
+    }
+}
 
 
 
@@ -28,8 +45,14 @@ public class taks1 {
     public static void main(String[] args){
         Mobile aftab=new Aftab();
         aftab.call();
+        aftab.message();
         aftab.openYouTube();
         Mobile priya= new Priya();
+        priya.message();
         priya.call();
+        Mobile abdul_sir= new Abdul_sir();
+        abdul_sir.message();
+        Mobile mobile=new Aftab();
+        mobile.broadcast();
     }
 }
