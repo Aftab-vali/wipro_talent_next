865b1ddf8948d46c391cdddf23d7b6507590ba9d
mohammed-aftab <mohammedaftabvali@gmail.com>
2025-07-30 10:20:55 +0530

    30/07/2025 10:20AM commit

---
+++ src/Day_3/GetPropertiesDemo.java
@@ -0,0 +1,11 @@
+package Day_3;
+
+
+import java.util.*;
+class GetPropertiesDemo {
+    public static void main (String [] args) {
+        Properties x = System. getProperties () ;
+        x. list (System.out);
+
+    }
+}
